ac468bbaca33b7b58c821a73559e8d2344f01cbc
dileephemachandranharman <dileep.hemachandran@harman.com>
2025-02-20 12:37:07 +0530

    Removed unused lib
    Removed unwanted libraries from SDK module

--- androidVehicleConnectSDK/build.gradle.kts
+++ androidVehicleConnectSDK/build.gradle.kts
@@ -36,35 +36,21 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
 }
 
 dependencies {
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.compose.ui:ui-graphics:1.7.8")
-    implementation("androidx.compose.material3:material3:1.3.1")
-
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.12.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("net.openid:appauth:0.11.1")
     implementation("androidx.multidex:multidex:2.0.1")
 
-    implementation("com.google.dagger:dagger:2.50")
-    implementation("com.google.dagger:dagger-android:2.50")
-    implementation("com.google.dagger:dagger-android-support:2.50")
-    kapt("com.google.dagger:dagger-android-processor:2.50")
-    kapt("com.google.dagger:dagger-compiler:2.50")
+    implementation("com.google.dagger:dagger:2.55")
+    implementation("com.google.dagger:dagger-android:2.55")
+    implementation("com.google.dagger:dagger-android-support:2.55")
+    kapt("com.google.dagger:dagger-android-processor:2.55")
+    kapt("com.google.dagger:dagger-compiler:2.55")
 }
