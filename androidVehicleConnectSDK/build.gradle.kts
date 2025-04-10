a6d6f247d02fd22ade60b7b70f324964104748b7
dileephemachandranharman <dileep.hemachandran@harman.com>
2025-04-10 11:43:31 +0530

    updated subkey id

--- androidVehicleConnectSDK/build.gradle.kts
+++ androidVehicleConnectSDK/build.gradle.kts
@@ -107,7 +107,7 @@ afterEvaluate {
 }
 
 signing {
-    useInMemoryPgpKeys(System.getenv("GPG_KEY_ID"), System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSPHRASE"))
+    useInMemoryPgpKeys(System.getenv("GPG_SUBKEY_ID"), System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSPHRASE"))
     sign(publishing.publications)
 }
 
