a468ffc999385c533a07087d73b478b07e77dbb8
dileephemachandranharman <dileep.hemachandran@harman.com>
2025-04-11 17:29:07 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ nexusPublishing {
         sonatype {
             username = System.getenv("OSSRH_USERNAME")
             password = System.getenv("OSSRH_PASSWORD")
-            stagingProfileId = System.getenv("GPG_KEY_ID")
+            stagingProfileId = "ossrh"
             nexusUrl.set(uri(("https://oss.sonatype.org/service/local/staging/deploy/maven2/")))
             snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots"))
         }
