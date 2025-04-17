3155087dfe300aeca43b7d05968e45bad2970e01
dileephemachandranharman <dileep.hemachandran@harman.com>
2025-04-17 11:44:51 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ project.allprojects {
 nexusPublishing {
     repositories {
         sonatype {
-            stagingProfileId = "ossrh"
+            stagingProfileId = "Releases"
             username = System.getenv("OSSRH_USERNAME")
             password = System.getenv("OSSRH_PASSWORD")
             nexusUrl.set(uri("https://oss.sonatype.org/service/local/"))
