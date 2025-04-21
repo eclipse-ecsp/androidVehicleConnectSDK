f0eab84c8ea8d2b1865e3d09627058a1f4991b09
dileephemachandranharman <dileep.hemachandran@harman.com>
2025-04-21 12:06:22 +0530

    updated build gradles

--- androidVehicleConnectSDK/build.gradle.kts
+++ androidVehicleConnectSDK/build.gradle.kts
@@ -126,6 +126,18 @@ publishing {
             sign(this)
         }
     }
+
+    repositories {
+        maven {
+            name = "Releases"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     id("org.jetbrains.dokka") version ("1.9.10")
     id("org.cyclonedx.bom") version ("2.2.0")
     id("org.jlleitschuh.gradle.ktlint") version ("12.2.0")
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+//    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 subprojects {
@@ -64,12 +64,12 @@ project.allprojects {
     }
 }
 
-nexusPublishing {
+/*nexusPublishing {
     repositories {
         sonatype {
             username = System.getenv("OSSRH_USERNAME")
             password = System.getenv("OSSRH_PASSWORD")
-            nexusUrl.set(uri("https://oss.sonatype.org/service/local/repositories/Releases/"))
+            nexusUrl.set(uri("https://oss.sonatype.org/service/local/"))
         }
     }
-}
+}*/
