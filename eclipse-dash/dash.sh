60914ecec025e1953c3b6506db1e6c33b648c38a
dileephemachandranharman <dileep.hemachandran@harman.com>
2025-04-22 16:46:56 +0530

    Updated license workflow

--- eclipse-dash/dash.sh
+++ eclipse-dash/dash.sh
@@ -7,16 +7,8 @@ chmod +x 'gradlew'
 echo 'Checking dependencies for SDK module'
 './gradlew' 'androidVehicleConnectSDK:dependencies' | 'grep' '-Poh' "(?<=\-\-\- ).*" | 'grep' '-Pv' "\([c\*]\)" | 'grep' '-Pv' "\([n\*]\)" | 'perl' '-pe' 's/([\w\.\-]+):([\w\.\-]+):(?:[\w\.\-]+ -> )?([\w\.\-]+).*$/$1:$2:$3/gmi;t' | 'sort' -u > 'SdkDependenciesItem'
 
-echo 'Checking dependencies for SDK implemented test app Module'
-'./gradlew' 'app:dependencies' | 'grep' '-Poh' "(?<=\-\-\- ).*" | 'grep' '-Pv' "\([c\*]\)" | 'grep' '-Pv' "\([n\*]\)" | 'perl' '-pe' 's/([\w\.\-]+):([\w\.\-]+):(?:[\w\.\-]+ -> )?([\w\.\-]+).*$/$1:$2:$3/gmi;t' | 'sort' -u | 'grep' '-v' 'project :androidVehicleConnectSDK' > 'SdkTestDependenciesItem'
-
-
 echo 'Tool checking for SDK dependencies'
+echo "File Path, $PWD "
 
 chmod +x $PWD/eclipse-dash/dash.jar $PWD/SdkDependenciesItem
 java -jar $PWD/eclipse-dash/dash.jar $PWD/SdkDependenciesItem -summary $PWD/SDK_NOTICE.md
-
-echo 'Tool checking for SDK test case application dependencies'
-
-chmod +x $PWD/eclipse-dash/dash.jar $PWD/SdkTestDependenciesItem
-java -jar $PWD/eclipse-dash/dash.jar $PWD/SdkTestDependenciesItem -summary $PWD/SDK_TEST_NOTICE.md
